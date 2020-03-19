d2205e073aac4f4822ea7fcd4c4a7b02fd03b1e3
jahidratul <jahidratul@gmail.com>
2020-03-19 15:08:03 +0600

    login registration ui updated

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:bestfriend/registerPage.dart';
+import 'package:flutter/material.dart';
+import 'loginPage.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'bestFriend',
+      initialRoute: LoginPage.id,
+      routes: {
+
+        LoginPage.id: (BuildContext context) => LoginPage(),
+        RegisterPage.id: (BuildContext context) => RegisterPage(),
+
+      },
+    );
+  }
+}
---
+++ lib/registerPage.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+class RegisterPage extends StatefulWidget {
+  static String id = 'registerPage';
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<RegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: ListView(
+            children: <Widget>[
+              Container(
+                child: Column(
+                  children: <Widget>[
+
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(left: 10, top: 30, right: 30),
+                      child: Row(
+                        children: <Widget>[
+                          Container(
+                            child: Image.asset('images/bflogop.png'),
+                            height: 80.0,
+                          ),
+                          Text(
+                            ' Register ',
+                            style: TextStyle(
+                              fontSize: 40.0,
+                              fontWeight: FontWeight.w900,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Card(
+                      margin: EdgeInsets.only(left: 30, top: 30, right: 30),
+                      elevation: 10,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(40))),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Input Name',
+                          labelText: 'Enter your Name',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(40)),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const Card(
+                      margin: EdgeInsets.only(left: 30, top: 30, right: 30),
+                      elevation: 10,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(40))),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Input Mobile No',
+                          labelText: 'Enter your Mobile No',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(40)),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const  Card(
+                      margin: EdgeInsets.only(left: 30, top: 30, right: 30),
+                      elevation: 10,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(40))),
+                      child: TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Input Password',
+                          labelText: 'Enter your Password',
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(40)),
+                          ),
+                        ),
+                      ),
+                    ),
+                    const   SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      width: double.infinity,
+                      margin: const EdgeInsets.only(left: 30, top: 30, right: 30),
+                      child: RaisedButton(
+                        elevation: 10,
+                        color: Colors.pinkAccent,
+                        padding: const EdgeInsets.symmetric(vertical: 16),
+                        shape: const RoundedRectangleBorder(
+                            borderRadius:
+                            BorderRadius.all(Radius.circular(40))),
+                        onPressed: () {},
+                        child: Text(
+                          'Sign Up',
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+
+
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
